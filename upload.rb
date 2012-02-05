5718596c8ebffc274271843a9996283c531ca985
Bryan McKelvey <bryan.mckelvey@gmail.com>
2012-02-05 19:05:15 +0900

    Better GitHub upload that almost works

--- upload.rb
+++ upload.rb
@@ -8,7 +8,19 @@ zip_files     = Dir[File.join(this_path, PROJECT_NAME, "bin", "Release",
                               "#{PROJECT_NAME}-*.zip")]
 
 zip_files.each do |zip_file|
-  puts upload_script
-  puts "Uploading #{zip_file}..."
-  system "ruby \"%s\" \"%s\" %s" % [upload_script, zip_file, REPO_NAME]
+  # Get the basename and build a description
+  basename = File.basename(zip_file)
+  if basename =~ /x86/
+    description = "The latest 32-bit version"
+  elsif basename =~ /x64/
+    description = "The latest 64-bit version"
+  end
+  puts "Uploading #{basename}..."
+
+  # Upload the files
+  Dir.chdir(File.dirname(zip_file)) do
+    system "ruby \"%s\" \"%s\" %s \"%s\"" % [upload_script, basename, REPO_NAME, description]
+  end
 end
+
+# {"expirationdate":"2112-02-05T09:32:38.000Z","signature":"HfasCLOJ1CWIRvbeXaD3wLseozk=","prefix":"downloads/brymck/finansu","redirect":false,"acl":"public-read","mime_type":"application/zip","accesskeyid":"1DWESVTPGHQVTX38V182","path":"downloads/brymck/finansu/FinAnSu-0.9.4_x86.zip","bucket":"github","policy":"ewogICAgJ2V4cGlyYXRpb24nOiAnMjExMi0wMi0wNVQwOTozMjozOC4wMDBaJywKICAgICdjb25kaXRpb25zJzogWwogICAgICAgIHsnYnVja2V0JzogJ2dpdGh1Yid9LAogICAgICAgIHsna2V5JzogJ2Rvd25sb2Fkcy9icnltY2svZmluYW5zdS9GaW5BblN1LTAuOS40X3g4Ni56aXAnfSwKICAgICAgICB7J2FjbCc6ICdwdWJsaWMtcmVhZCd9LAogICAgICAgIHsnc3VjY2Vzc19hY3Rpb25fc3RhdHVzJzogJzIwMSd9LAogICAgICAgIFsnc3RhcnRzLXdpdGgnLCAnJEZpbGVuYW1lJywgJyddLAogICAgICAgIFsnc3RhcnRzLXdpdGgnLCAnJENvbnRlbnQtVHlwZScsICcnXQogICAgXQp9"}
